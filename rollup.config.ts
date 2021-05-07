1f6fe34c4cc05ea17eae5e8e3a8ac6441189bba1
Chris Corbett <ccorbett@amplience.com>
2021-05-07 18:08:15 +0100

    fix: renaming core to sdk

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'dc-visualization-core'
+const libraryName = 'dc-visualization-sdk'
 
 export default {
   input: `src/${libraryName}.ts`,
